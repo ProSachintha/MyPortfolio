36d5d0862cf57e956320bd503cdffc3d7c08f609
sachintha <sachintha.s.gunawardhana@gmail.com>
2025-02-07 01:47:17 +0530

    Add my data to Resume  page

--- app/Resume/page.jsx
+++ app/Resume/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-import {FaHtml5,FaCss3,FaJs,FaReact,FaFigma,FaNodeJs} from "react-icons/fa"
-import {SiTailwindcss,SiNextdotjs} from "react-icons/si"
+import {FaHtml5,FaCss3,FaJs,FaReact,FaFigma,FaNodeJs,FaPython,FaJava,FaPhp,FaAngular,FaGit,FaGithub} from "react-icons/fa"
+import {SiTailwindcss,SiNextdotjs,SiExpress,SiMysql,SiAdobephotoshop} from "react-icons/si"
 
 const about = {
   title:"About Me",
@@ -46,8 +46,124 @@ const about = {
 };
 
 const experience = {
-  icon: '/'
+  icon: '',
+  title:"My Experience",
+  description:"",
+  item:[
+    {
+      company: "Pure-X Software Solution - Freelance",
+      position:"Full Stack Developer",
+      duration:"2022- Present"
+    },
+    {
+      company: "Sachini Trade Center",
+      position:"Sales Man",
+      duration:"2021- 2022"
+    },
+    {
+      company: "JLanka Technologies",
+      position:"Admin Officer",
+      duration:"2020- 2021"
+    }
+  ]
 }
+const education = {
+  icon:"",
+  title:"My Education",
+  description:"",
+  item:[
+    {
+      institute:"Vocational Training Authority",
+      program:"ICT NVQ Level 5",
+      duration:"2021 Hold"
+    },
+    {
+      institute:"Open University of Sri Lanka",
+      program:"Bachelor of Software Engineering (Honours)",
+      duration:"2022 - Present"
+    },
+    
+  ]
+}
+const skils = {
+  title: "My Skils",
+  description:"Thease Days I foucus To Web Development With MERN Stack.But I have Some Another Skils.",
+  skilset:[
+    {
+      icon:<FaHtml5/>,
+      name:"HTML"
+    },
+    {
+      icon:<FaCss3/>,
+      name:"CSS"
+    },
+    {
+      icon:<FaJs/>,
+      name:"Java Script"
+    },
+    {
+      icon:<FaNodeJs/>,
+      name:"NodeJS"
+    },
+    {
+      icon:<FaReact/>,
+      name:"ReactJS"
+    },
+    {
+      icon:<SiNextdotjs/>,
+      name:"NextJS"
+    },
+    {
+      icon:<FaAngular/>,
+      name:AngularJs
+    },
+    {
+      icon:<FaPhp/>,
+      name:"PHP"
+    },
+    {
+      icon:<SiExpress/>,
+      name:"ExpressJs"
+    },
+    {
+      icon:<SiMysql/>,
+      name:"MySQL"
+    },
+    {
+      icon:<SiTailwindcss/>,
+      name:"Tailwind CSS"
+    },
+    {
+      icon:<FaJava/>,
+      name:"Java"
+    },
+    {
+      icon:<FaPython/>,
+      name:"Python"
+    },
+    {
+      icon:<FaGit/>,
+      name:"Git"
+    },
+    {
+      icon:<FaGithub/>,
+      name:"GitHub"
+    },
+    {
+      icon:<FaFigma/>,
+      name:"Figma"
+    },
+    {
+      icon:<SiAdobephotoshop/>,
+      name:"Photoshop"
+    },
+  ]
+}
+
+import {Tabs,TabsContent,TabsList,TabsTrigger} from '@/components/ui/tabs';
+import { Tooltip,TooltipContent,TooltipProvider,TooltipTrigger } from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
 
 
 const Resume = () => {
