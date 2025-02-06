9d91348d66251751f6e56c6ed5c1fec64dd38be6
sachintha <sachintha.s.gunawardhana@gmail.com>
2025-02-06 07:55:27 +0530

    Starting Resume  page

--- app/Resume/page.jsx
+++ app/Resume/page.jsx
@@ -1,3 +1,53 @@
+"use client";
+
+import {FaHtml5,FaCss3,FaJs,FaReact,FaFigma,FaNodeJs} from "react-icons/fa"
+import {SiTailwindcss,SiNextdotjs} from "react-icons/si"
+
+const about = {
+  title:"About Me",
+  description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, excepturi eaque! Reprehenderit sed amet nostrum. Nobis, ratione!0",
+  info:[
+    {
+    fieldname:"Name",
+    fieldvalue:"B.V.G.S.S.Gunawardhana"
+
+  },
+    {
+    fieldname:"Phone",
+    fieldvalue:"(+94) 72 5808 419"
+
+  },
+    {
+    fieldname:"Experience",
+    fieldvalue:"2+ Years"
+
+  },
+    {
+    fieldname:"Natioanality",
+    fieldvalue:"Sri Lankan"
+
+  },
+    {
+    fieldname:"Email",
+    fieldvalue:"sachintha.s.gunawardhana@gmail.com"
+
+  },
+    {
+    fieldname:"Languages",
+    fieldvalue:"Sinhala , English"
+
+  },
+    {
+    fieldname:"Freelance",
+    fieldvalue:"Available"
+
+  }
+]
+};
+
+const experience = {
+  icon: '/'
+}
 
 
 const Resume = () => {
