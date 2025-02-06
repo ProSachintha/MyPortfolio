ed55eec35806ad873218575bea8464d6a1506727
sachintha <sachintha.s.gunawardhana@gmail.com>
2025-02-06 05:33:25 +0530

    Starting Services page

--- app/Services/page.jsx
+++ app/Services/page.jsx
@@ -1,5 +1,28 @@
 
+"use client";
+import {BsArrowDownRight} from "react-icons/bs";
+import Link from "next/link";
 
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:"",
+    href:""
+  },
+  {
+    num: "02",
+    title: "Software Development",
+    description:"",
+    href:""
+  },
+  {
+    num: "03",
+    title: "UI/UX Design",
+    description:"",
+    href:""
+  },
+]
 const Services = () => {
   return (
     <div>Services page</div>
